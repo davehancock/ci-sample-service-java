a60ad2f438918b5c558a59cd1959accaf0f427f5
Dave <daves125125@gmail.com>
2017-07-04 23:38:59 +0100

    jenkins

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,14 @@
+node {
+
+    stage("Main build") {
+
+        checkout scm
+
+        // https://go.cloudbees.com/docs/cloudbees-documentation/cje-user-guide/index.html#docker-workflow
+        docker.image('openjdk:8-jdk').inside {
+            sh './gradlew clean test'
+            sh './gradlew build'
+        }
+    }
+
+}
